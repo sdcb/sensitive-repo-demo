464fb2f2217632c5ad626f6dae293e9b8ec61b00
sdcb <sdflysha@qq.com>
2019-08-08 22:18:54 +0800

    add aws secret key in Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace sensitive_repo_demo
     {
         static void Main(string[] args)
         {
+			string awsAccessKey = "pwiCZSMCIcM6+q+h";
+			string awsSecretKey = "861YUaeCHqzaS5OX+OmAK1XD37kmQhA2";
             Console.WriteLine("Hello World!");
         }
     }
